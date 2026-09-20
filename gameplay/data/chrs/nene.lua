1df83f98657920f1190a785deed76f022d6bbfe8
Mily_0 <browserbma@gmail.com>
2026-09-20 18:19:26 -0300

    abot wip

--- gameplay/data/chrs/nene.lua
+++ gameplay/data/chrs/nene.lua
@@ -1,4 +1,5 @@
-
+local nX = getProperty('gf.x');
+local nY = getProperty('gf.y');
 
 function onLoad()
     createAbot();
@@ -12,13 +13,14 @@ end
 
 
 function createAbot()
-    makeAtlasSprite('abot', -5.5, -126, 'chrs/nene/abot/main/system');
-    addAnim('abot', 'idle', 'abot speaker', 24, false);
+    makeAnimatedLuaSprite('abot', 'chrs/nene/abot/main/abot-sparrow', nX, nY);
+    addAnimationByPrefix('abot', 'idle', 'abot', 24, false);
+    setScrollFactor('abot', 0.95, 0.95);
         
     addLuaSprite('abot', false);
 end
 
 
 function playAbot()
-    playAnim('abot', 'idle', true);
+    --playAnim('abot', 'idle', true);
 end
